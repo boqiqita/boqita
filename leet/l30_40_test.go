082e939727b43a333687a2c86a3fa7c8d126f28d
zhaobo.cc <zhaobo.cc@bytedance.com>
2024-06-21 11:57:44 +0800

    20240621
    Change-Id: I05c58b30b27506d07f9b45d85e4c9978ba84504e

--- leet/l30_40_test.go
+++ leet/l30_40_test.go
@@ -5,6 +5,40 @@ import (
 	"testing"
 )
 
+func Test_l34(t *testing.T) {
+	assert.Equal(t, []int{3, 4}, searchRange([]int{5, 7, 7, 8, 8, 10}, 8))
+	assert.Equal(t, []int{-1, -1}, searchRange([]int{5, 7, 7, 8, 8, 10}, 6))
+	assert.Equal(t, []int{-1, -1}, searchRange([]int{5, 7, 7, 8, 8, 10}, 4))
+	assert.Equal(t, []int{-1, -1}, searchRange([]int{5, 7, 7, 8, 8, 10}, 11))
+	assert.Equal(t, []int{-1, -1}, searchRange([]int{}, 0))
+}
+
+func searchRange(nums []int, target int) []int {
+	length := len(nums)
+	l, r, mid := 0, length-1, -1
+	for l < r {
+		mid = l + (r-l)/2
+		if nums[mid] == target {
+			l, r = mid-1, mid+1
+			for l > -1 && nums[l] == target {
+				l--
+			}
+			for r < length && nums[r] == target {
+				r++
+			}
+			return []int{l + 1, r - 1}
+		} else if l == mid || r == mid {
+			return []int{-1, -1}
+		} else if nums[mid] < target {
+			l = mid
+		} else {
+			r = mid
+		}
+	}
+
+	return []int{-1, -1}
+}
+
 func Test_l35(t *testing.T) {
 	assert.Equal(t, 3, searchInsert([]int{1, 3, 4, 5, 6, 8, 10}, 5))
 	assert.Equal(t, 2, searchInsert([]int{1, 4, 6, 7, 8, 9}, 6))
